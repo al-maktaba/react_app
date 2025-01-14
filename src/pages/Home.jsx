e6ac0482811c7455c93cbadd11d5f53d6fcf7060
amina <amina.hafarova@valthsolutions.com>
2025-01-14 21:36:17 +0100

    add video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import flowersImg from "../assets/images/flowers.png"
+import ReactPlayer from 'react-player/youtube';
 
 const Home = () => {
     return (
@@ -10,11 +11,18 @@ const Home = () => {
                 alt='An image of flowers'
                 height={400}
             />
-            <img
+            {/* <img
                 style={{ marginTop: 42 }}
                 src={require("../assets/images/flowers.png")}
                 alt='An image of flowers'
                 height={400}
+            /> */}
+            <ReactPlayer
+                style={{ marginTop: 42, display: "block", margin: "0 auto" }}
+                url="https://www.youtube.com/watch?v=i2EcUZzk548"
+                playing={true}
+                volume={0.1}
+                width={540}
             />
         </div>
     );
